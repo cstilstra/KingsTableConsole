5247319635143dfd9674d12c78acf0551b72f971
c.s.tilstra <cstilstra@gmail.com>
2018-04-17 20:47:34 -0700

    create complete internal representation of board, place all playable pieces onto board.

--- KingsTableConsoleEdition/MainRules.cs
+++ KingsTableConsoleEdition/MainRules.cs
@@ -5,13 +5,18 @@ namespace KingsTableConsoleEdition
     public class MainRules : IRules
     {
         Board board;
+        Board emptyBoard;
         int[,] corners;
 
-        char goalChar;
+        char goalChar, throneChar, attackerChar, defenderChar, kingChar;
 
         public MainRules()
         {
             goalChar = 'X';
+            throneChar = '+';
+            attackerChar = 'A';
+            defenderChar = 'D';
+            kingChar = 'K';
         }
 
         public void StartNewGame(Board newBoard)
@@ -21,7 +26,11 @@ namespace KingsTableConsoleEdition
             {
                 FindCorners();
                 MarkCornersAsGoals();
+                MarkThrone();
+                emptyBoard = board;
                 PlaceAttackers();
+                PlaceDefenders();
+                PlaceKing();
             }else{
                 Console.WriteLine("");
                 Console.WriteLine("The current board is not compatible with this rule type");
@@ -45,28 +54,68 @@ namespace KingsTableConsoleEdition
             }
         }
 
+        void MarkThrone()
+        {
+            int[] position = { 5, 5};
+            board.SetPositionToValue(position, throneChar);
+        }
+
         void PlaceAttackers()
         {
             int[] position;
             for (int i = 3; i < 8; i++)
             {
                 position = new int[] { i, 0 };
-                board.SetPositionToValue(position, 'A');
+                board.SetPositionToValue(position, attackerChar);
                 position = new int[] { i, 10 };
-                board.SetPositionToValue(position, 'A');
+                board.SetPositionToValue(position, attackerChar);
                 position = new int[] { 0, i };
-                board.SetPositionToValue(position, 'A');
+                board.SetPositionToValue(position, attackerChar);
                 position = new int[] { 10, i };
-                board.SetPositionToValue(position, 'A');
+                board.SetPositionToValue(position, attackerChar);
             }
             position = new int[] { 5, 1 };
-            board.SetPositionToValue(position, 'A');
+            board.SetPositionToValue(position, attackerChar);
             position = new int[] { 5, 9 };
-            board.SetPositionToValue(position, 'A');
+            board.SetPositionToValue(position, attackerChar);
             position = new int[] { 1, 5 };
-            board.SetPositionToValue(position, 'A');
+            board.SetPositionToValue(position, attackerChar);
             position = new int[] { 9, 5 };
-            board.SetPositionToValue(position, 'A');
+            board.SetPositionToValue(position, attackerChar);
+        }
+
+        void PlaceDefenders()
+        {
+            int[] position = new int[] { 3 , 5 };
+            board.SetPositionToValue(position, defenderChar);
+            position = new int[] { 4, 4 };
+            board.SetPositionToValue(position, defenderChar);
+            position = new int[] { 4, 5 };
+            board.SetPositionToValue(position, defenderChar);
+            position = new int[] { 4, 6 };
+            board.SetPositionToValue(position, defenderChar);
+            position = new int[] { 5, 3 };
+            board.SetPositionToValue(position, defenderChar);
+            position = new int[] { 5, 4 };
+            board.SetPositionToValue(position, defenderChar);
+            position = new int[] { 5, 6 };
+            board.SetPositionToValue(position, defenderChar);
+            position = new int[] { 5, 7 };
+            board.SetPositionToValue(position, defenderChar);
+            position = new int[] { 6, 4 };
+            board.SetPositionToValue(position, defenderChar);
+            position = new int[] { 6, 5 };
+            board.SetPositionToValue(position, defenderChar);
+            position = new int[] { 6, 6 };
+            board.SetPositionToValue(position, defenderChar);
+            position = new int[] { 7, 5 };
+            board.SetPositionToValue(position, defenderChar);
+        }
+
+        void PlaceKing()
+        {
+            int[] position = new int[] { 5, 5 };
+            board.SetPositionToValue(position, kingChar);
         }
     }
 }
